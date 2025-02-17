66179aaa5d48ad9e02508b52625539afa60c01d3
Tina Xia <tinaxia@Tinas-MacBook-Pro.local>
2025-02-17 01:55:43 -0500

    text decorations and background

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="hero">
       <div className="content">
-        <h1>Hi 👋 I'm Tina. I'm a software developer 💻 studying Computer Engineering
-          at Waterloo. I've interned at Shopify 🛒 and IBM 🌐. Feel free to drop
-          me an message 💌, scroll through my travel blog 🗺️, or check out my experiences 🚀.
+        <h1>Hi 👋 I'm <span className="bold">Tina.</span> I'm a software developer 💻 studying <span className="highlightWaterloo">Computer Engineering</span> at the 
+        University of Waterloo. I've interned at <span className="highlightShopify">Shopify</span> 🛒 and IBM 🌐. Feel free to <span className="underline">drop
+        me an message</span> 💌, scroll through my travel blog 🗺️, or check out my experiences 🚀.
         </h1>
       </div>
       <div className="content">
