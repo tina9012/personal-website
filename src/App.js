d6984f29856951fcdb895b58cc06f3aa584f4ec5
Tina Xia <tinaxia@eduroam-campus-10-36-205-146.campus-dynamic.uwaterloo.ca>
2025-02-17 20:52:35 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import coverImage from './assets/parents.png'
 function App() {
   return (
     <div className="hero">
+
+
       <div className="content">
-        <h1>Hi 👋 I'm <span className="bold">Tina.</span> I'm a software developer 💻 studying <span className="highlightWaterloo">Computer Engineering</span> at the 
-        University of Waterloo. I've interned at <span className="highlightShopify">Shopify</span> 🛒 and IBM 🌐. Feel free to <span className="underline">drop
-        me an message</span> 💌, scroll through my travel blog 🗺️, or check out my experiences 🚀.
+        <h1>Hi 👋 I'm <span className="bold">Tina.</span> I'm a software developer studying <span className="highlightWaterloo">Computer Engineering</span> at the 
+        University of Waterloo. I've interned at <span className="shopify">Shopify and IBM.</span> Feel free to <span className="underline">drop
+        me an message</span>, scroll through my travel blog, or check out <span className="ibm">my experiences 🚀</span>
         </h1>
       </div>
       <div className="content">
         <img src={coverImage} alt="Description"></img>
       </div>
+
     </div>
   );
 }
